f711431d88a1c22bb92931bf3bdce1bcd2da8480
kakoi-to-pirat <kakoi-to.pirat@yandex.ru>
2019-06-04 12:16:36 +0300

    add Query and Responce to UserController's Login method

--- src/versions/v1/controllers/user.controller.ts
+++ src/versions/v1/controllers/user.controller.ts
@@ -1,14 +1,23 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Post, Req, Res, Query, HttpStatus } from '@nestjs/common';
 import { UserService } from '../../../common/user/user.service';
+import { Request, Response } from 'express';
 
 @Controller('v1/user')
 export class UserController {
   constructor(
     private readonly userService: UserService,
-  ) {}
+  ) { }
 
   @Post('login')
-  login() {
-    return true;
+  login(
+    @Query() query,
+    @Res() res: Response
+  ) {
+    console.log(HttpStatus);
+
+    res.status(HttpStatus.UNAUTHORIZED).json({
+      "statusCode": 401,
+      "error": "Unauthorized"
+    });
   }
 }
